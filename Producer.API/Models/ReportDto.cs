55aca73e28049fc0658a31df5641d593cc12795e
ahmetturkben <53624843+ahmetturkben@users.noreply.github.com>
2021-02-20 19:34:35 +0300

    Add project files.

---
+++ Producer.API/Models/ReportDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producer.API.Models
+{
+    public class ReportDto
+    {
+        public int MyProperty { get; set; }
+    }
+}
